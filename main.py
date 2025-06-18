3ed2380b3b1dc813abef8e3ff618ace76195dd07
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-18 18:48:57 +0100

    herlisreal

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
-# Initialize dictionary
-test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+country_code = { 'India' : '0091',
+                'Australia' : '0025',
+                'Nepal' : '00977'}
 
-# printing original dictionary
-print("The original dictionary : " + str(test_dict))
+# search dictionary for country code of India
+print("Country code for India -")
+print(country_code.get('India', 'Not Found'))
 
-# Initialize value
-K = 2
-
-# Using loop
-# Selective key values in dictionary
-res = 0
-for key in test_dict:
-    if test_dict[key] == K:
-        res = res + 1
-
-# printing result
-print("Frequency of K is : " + str(res))
+# search dictionary for country code of Japan
+print("Country code for Japan -")
+print(country_code.get('Japan', 'Not Found'))
