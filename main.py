864e67888cb6ba4bf7f9ab4fc91b523ae326282f
Usernameorsmth <simidarawilliams@gmial.com>
2025-06-18 18:44:28 +0100

    Slither

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-student_data = {
-    'id1': {
-        'name': ['Sara'],
-        'class': ['V'],
-        'subject_integration': ['english', 'math', 'science']
-    },
-    'id2': {
-        'name': ['David'],
-        'class': ['V'],
-        'subject_integration': ['english', 'math', 'science']
-    },
-    'id3': {
-        'name': ['Sara'],
-        'class': ['V'],
-        'subject_integration': ['english', 'math', 'science']
-    },
-    'id4': {
-        'name': ['Surya'],
-        'class': ['V'],
-        'subject_integration': ['english', 'math', 'science']
-    },
-}
-result = {}
+# Initialize dictionary
+test_dict = {'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
 
-for key,value in student_data.items():
-    if value not in result.values():
-        result[key] = value
+# printing original dictionary
+print("The original dictionary : " + str(test_dict))
 
-print(result)
+# Initialize value
+K = 2
+
+# Using loop
+# Selective key values in dictionary
+res = 0
+for key in test_dict:
+    if test_dict[key] == K:
+        res = res + 1
+
+# printing result
+print("Frequency of K is : " + str(res))
